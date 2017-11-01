4fa319806165bcd28553da8938fab2aaaca80efb
dmitriy012 <dmitriyarkhipchuk@yahoo.com>
2017-11-01 15:01:10 -0700

    player class which holds the players position

---
+++ Monopoly/src/main/java/Player.java
@@ -0,0 +1,14 @@
+public class Player
+{
+    private Cell position;
+
+    public Cell getPosition()
+    {
+        return this.position;
+    }
+
+    public void setPosition(Cell newPosition)
+    {
+        this.position = newPosition;
+    }
+}
