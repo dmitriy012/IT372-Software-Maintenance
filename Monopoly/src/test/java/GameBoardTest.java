85919d41758e7131d3e7d76e9c73493c1d1bd4f2
dmitriy012 <dmitriyarkhipchuk@yahoo.com>
2017-11-01 14:39:54 -0700

    added a test case to test the Go cell

--- Monopoly/src/test/java/GameBoardTest.java
+++ Monopoly/src/test/java/GameBoardTest.java
@@ -10,9 +10,16 @@ public class GameBoardTest extends TestCase
     public void testAddCell()
     {
         GameBoard gameBoard = new GameBoard();
-        assertEquals(0,gameBoard.getCellNumber());
+        assertEquals(1,gameBoard.getCellNumber());
         Cell cell = new Cell();
         gameBoard.addCell(cell);
-        assertEquals(1,gameBoard.getCellNumber());
+        assertEquals(2,gameBoard.getCellNumber());
+    }
+
+    public void testFirstCell()
+    {
+        GameBoard gameBoard = new GameBoard();
+        Cell firstCell = gameBoard.getCell(0);
+        assertSame(GoCell.class, firstCell.getClass());
     }
 }
